7ccf0cb62c0f36015d66f88dd3e9fa3fd2a8374a
Ava Schmidt <avamschmidt07@gmail.com>
2024-09-25 08:08:56 -0500

    Started project!

---
+++ ClickerGameStoreSchmidt/StoreViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+  
+
+}
--- ClickerGameStoreSchmidt/ViewController.swift
+++ ClickerGameStoreSchmidt/ViewController.swift
@@ -1,19 +1,29 @@
-//
-//  ViewController.swift
-//  ClickerGameStoreSchmidt
-//
-//  Created by AVA SCHMIDT on 9/25/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    var points = 0.0
+    var multiplier = 1.0
+    var add = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+        
+        
+    @IBAction func storeTapAction(_ sender: UITapGestureRecognizer) {
+        print("Store Clicked")
+        performSegue(withIdentifier: "storeSegue", sender: self)
+    }
+    
+    @IBAction func edTapAction(_ sender: UITapGestureRecognizer) {
+        points = points + (add * multiplier)
+    }
+    
+        
+    
+    
 
 }
 
