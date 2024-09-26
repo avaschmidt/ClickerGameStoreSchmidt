3f69e6a9cc3041b59b313df56570dffe2dbae955
Ava Schmidt <ava.schmidt25@d155.org>
2024-09-26 05:40:13 -0700

    Finished

--- ClickerGameStoreSchmidt/StoreViewController.swift
+++ ClickerGameStoreSchmidt/StoreViewController.swift
@@ -1,12 +1,55 @@
 import UIKit
 
 class StoreViewController: UIViewController {
+    
+    @IBOutlet weak var powerUpOutlet: UILabel!
 
+    @IBOutlet weak var errorOutlet: UILabel!
+    
+    @IBOutlet weak var pointsLabelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+            
+        pointsLabelOutlet.text = "Points: \(AppData.points)"
+        powerUpOutlet.text = "Multiplier: \(AppData.multiplier)x Add: \(AppData.add)+"
+        
       
     }
+    
+    
+    @IBAction func AddMoreAction(_ sender: UIButton) {
+        
+        if AppData.points >= 20.0{
+            errorOutlet.text = ""
+            AppData.points = AppData.points - 20.0
+            AppData.add = AppData.add + 1.0
+        }
+        else{
+            errorOutlet.text = "Not Enough Points"
+        }
+        powerUpOutlet.text = "Multiplier: \(AppData.multiplier)x Add: \(AppData.add)+"
+        pointsLabelOutlet.text = "Points: \(AppData.points)"
+    }
+    
+    
+    
+    @IBAction func multiplyAction(_ sender: UIButton) {
+        if AppData.points >= 50.0{
+            errorOutlet.text = ""
+            AppData.points = AppData.points - 50.0
+            AppData.multiplier = AppData.multiplier + 1.0
+        }
+        else{
+            errorOutlet.text = "Not Enough Points"
+        }
+        powerUpOutlet.text = "Multiplier: \(AppData.multiplier)x Add: \(AppData.add)+"
+        pointsLabelOutlet.text = "Points: \(AppData.points)"
+    }
+    
+    
+    
+    
   
 
 }
--- ClickerGameStoreSchmidt/ViewController.swift
+++ ClickerGameStoreSchmidt/ViewController.swift
@@ -1,25 +1,41 @@
 import UIKit
 
+class AppData{
+    static var points = 0.0
+    static var multiplier = 1.0
+    static var add = 1.0
+}
+
 class ViewController: UIViewController {
 
-    var points = 0.0
-    var multiplier = 1.0
-    var add = 1.0
+    
+    @IBOutlet weak var pointsOutlet: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pointsOutlet.text = "Points: \(AppData.points)"
        
     }
+    
+    
+    @IBAction func tapEdAction(_ sender: UITapGestureRecognizer) {
+        print("Ed Clicked")
+        AppData.points = AppData.points + (AppData.add * AppData.multiplier)
+        pointsOutlet.text = "Points: \(AppData.points)"
         
-        
+    }
+    
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        pointsOutlet.text = "Points: \(AppData.points)"
+    }
+    
+    
     @IBAction func storeTapAction(_ sender: UITapGestureRecognizer) {
         print("Store Clicked")
         performSegue(withIdentifier: "storeSegue", sender: self)
     }
     
-    @IBAction func edTapAction(_ sender: UITapGestureRecognizer) {
-        points = points + (add * multiplier)
-    }
     
         
     
