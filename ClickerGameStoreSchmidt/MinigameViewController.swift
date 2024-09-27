9d0aef145f728c67f6e4ba84f8e8f3a76a99dda2
Ava Schmidt <ava.schmidt25@d155.org>
2024-09-26 18:23:25 -0700

    Final Commit

--- ClickerGameStoreSchmidt/MinigameViewController.swift
+++ ClickerGameStoreSchmidt/MinigameViewController.swift
@@ -1,6 +1,17 @@
 import UIKit
 
 class MinigameViewController: UIViewController {
+    
+    @IBOutlet weak var guitarOutlet: UIImageView!
+    
+    
+    @IBOutlet weak var bowOutlet: UIImageView!
+    
+    
+    @IBOutlet weak var marioOutlet: UIImageView!
+    
+    
+    
     @IBOutlet weak var edSillyOutlet: UIImageView!
     
     
@@ -15,14 +26,52 @@ class MinigameViewController: UIViewController {
         
     }
     @IBAction func edSillyDragAction(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc =  sender.location(in: view)
+        edSillyOutlet.center = pannedLoc
+        print("panning")
     }
     
     @IBAction func edHiDragAction(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc =  sender.location(in: view)
+        edHiOutlet.center = pannedLoc
+        print("panning")
     }
     
     
     @IBAction func edHandsUpDragAction(_ sender: UIPanGestureRecognizer) {
-        
+        var pannedLoc =  sender.location(in: view)
+        edHandsOutlet.center = pannedLoc
+        print("panning")
     }
+    
+
+    
+    @IBAction func marioDragAction(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc =  sender.location(in: view)
+        marioOutlet.center = pannedLoc
+        print("panning")
+    }
+    
+    
+    @IBAction func bowDragAction(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc =  sender.location(in: view)
+        bowOutlet.center = pannedLoc
+        print("panning")
+    }
+    
+
+    
+    
+    
+    
+    @IBAction func guitarDragAction(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc =  sender.location(in: view)
+        guitarOutlet.center = pannedLoc
+        print("panning")
+    }
+    
+    
+    
+    
     
 }
--- ClickerGameStoreSchmidt/StoreViewController.swift
+++ ClickerGameStoreSchmidt/StoreViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class StoreViewController: UIViewController {
     
+    @IBOutlet weak var minigameButtonOutlet: UIButton!
+    
     @IBOutlet weak var powerUpOutlet: UILabel!
 
     @IBOutlet weak var errorOutlet: UILabel!
@@ -56,20 +58,30 @@ class StoreViewController: UIViewController {
         if unlocked == false{
             if AppData.points >= 500.0{
                 unlocked = true
+        // Found out how to set title from Stack Overflow: https://stackoverflow.com/questions/26641571/how-to-change-button-text-in-swift-xcode-6
+                minigameButtonOutlet.setTitle("Click To Enter Minigame", for: .normal)
                 errorOutlet.text = ""
                 AppData.points = AppData.points - 500.0
                 AppData.multiplier = AppData.multiplier + 1.0
+                
             }
             else{
                 errorOutlet.text = "Not Enough Points"
             }
         }
         else{
-            errorOutlet.text = "Already Unlocked!"
+            print("Segue Performed")
+            performSegue(withIdentifier: "minigameSegue", sender: self)
         }
         powerUpOutlet.text = "Multiplier: \(AppData.multiplier)x Add: \(AppData.add)+"
         pointsLabelOutlet.text = "Points: \(AppData.points)"
     }
+    
+    
+   
+        
+    
+    
     
     
     
